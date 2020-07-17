6f645f338c93779d400b17bc3b70e21e36a6dce7
Shubham Bisht <shubham99bisht@gmail.com>
2020-07-17 19:44:16 +0530

    All pages content ready

--- Novasant/assets/js/main.js
+++ Novasant/assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: BizLand - v1.0.1
-* Template URL: https://bootstrapmade.com/bizland-bootstrap-business-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 !(function($) {
   "use strict";
 
@@ -65,28 +59,6 @@
     }
   });
 
-  // Navigation active state on scroll
-  var nav_sections = $('section');
-  var main_nav = $('.nav-menu, .mobile-nav');
-
-  $(window).on('scroll', function() {
-    var cur_pos = $(this).scrollTop() + 200;
-
-    nav_sections.each(function() {
-      var top = $(this).offset().top,
-        bottom = top + $(this).outerHeight();
-
-      if (cur_pos >= top && cur_pos <= bottom) {
-        if (cur_pos <= bottom) {
-          main_nav.find('li').removeClass('active');
-        }
-        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-      }
-      if (cur_pos < 300) {
-        $(".nav-menu ul:first li:first, .mobile-menu ul:first li:first").addClass('active');
-      }
-    });
-  });
 
   // Mobile Navigation
   if ($('.nav-menu').length) {
